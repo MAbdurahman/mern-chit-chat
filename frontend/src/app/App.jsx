706c1094e6c474964be2a89b32a24ee93f91dd96
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-25 07:07:01 -0600

    Add NotificationProvider to main.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,12 +1,15 @@
 import {Fragment} from 'react';
-import { Button } from "@/components/ui/button.jsx"
+import {Button} from '@/components/ui/button.jsx'
 
 export default function App() {
 
    return (
       <Fragment>
-         <h2 className="text-3xl text-center p-8 font-semibold">Vite React Project with Tailwind CSS v4 and Shadcn UI</h2>
-         <Button className="ml-4">Click Me</Button>
+         <h2 className="text-3xl text-center p-8 font-semibold">Vite React Project
+            with Tailwind CSS v4 and Shadcn UI</h2>
+         <div className="w-full max-w-7xl mx-auto p-8">
+            <Button>Click Me</Button>
+         </div>
       </Fragment>
 
    );
